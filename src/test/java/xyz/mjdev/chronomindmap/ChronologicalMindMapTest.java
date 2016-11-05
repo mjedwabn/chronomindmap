945972c235122a52b99c08ad2dc89f78e5cb0b68
Maciej Jedwabny <rudykot@gmail.com>
2016-11-05 21:09:41 +0100

    get list of all facts

--- src/test/java/xyz/mjdev/chronomindmap/ChronologicalMindMapTest.java
+++ src/test/java/xyz/mjdev/chronomindmap/ChronologicalMindMapTest.java
@@ -6,35 +6,56 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class ChronologicalMindMapTest {
 	@Test
+	public void newTimelineShallNotHaveFacts() {
+		Timeline timeline = new Timeline();
+		assertThat(timeline.getFacts(), is(empty()));
+	}
+
+	@Test
 	public void addDurationFact() {
 		Timeline timeline = new Timeline();
 		timeline.addFact("4E174", "4E176", new Fact("Fact"));
-		assertThat(timeline.facts("4E174", "4E176"), contains("Fact"));
+
+		assertThat(timeline.getFacts(), contains("Fact"));
+	}
+
+	@Test
+	public void addMultipleFacts() {
+		Timeline timeline = new Timeline();
+		timeline.addFact("4E20", "4E22", new Fact("Fact2"));
+		timeline.addFact("4E1", "4E2", new Fact("Fact1"));
+
+		assertThat(timeline.getFacts(), containsInAnyOrder("Fact1", "Fact2"));
 	}
 
 	private class Timeline {
 		private List<TimelineFact> facts = new ArrayList<>();
 
-		public List<String> facts(String from, String to) {
-			return facts.stream()
-					.filter(f -> f.isBetween(from, to))
-					.map(TimelineFact::getDescription)
-					.collect(Collectors.toList());
-		}
+//		public List<String> getFacts(String from, String to) {
+//			return facts.stream()
+//					.filter(f -> f.isBetween(from, to))
+//					.map(TimelineFact::getDescription)
+//					.collect(Collectors.toList());
+//		}
 
 		public void addFact(String from, String to, Fact fact) {
 			facts.add(new DurationFact(from, to, fact));
 		}
 
+		public List<String> getFacts() {
+			return facts.stream()
+					.map(TimelineFact::getDescription)
+					.collect(Collectors.toList());
+		}
+
 		private class DurationFact extends TimelineFact {
 			private final String from;
 			private final String to;
-
 
 			public DurationFact(String from, String to, Fact fact) {
 				super(fact);
@@ -50,9 +71,9 @@ public class ChronologicalMindMapTest {
 				this.fact = fact;
 			}
 
-			public boolean isBetween(String from, String to) {
-				return true;
-			}
+//			public boolean isBetween(String from, String to) {
+//				return true;
+//			}
 
 			public String getDescription() {
 				return fact.getDescription();
